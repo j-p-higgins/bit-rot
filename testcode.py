62bb2f1a207d9f333d7861478c85e98a3e628dee
Jonathan Higgins <jh03878@gmail.com>
2019-02-25 14:35:18 +0000

    First commit

---
+++ testcode.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import random
+import subprocess
+
+count = 30
+
+while True:
+ 
+ audio_file = "test.mp3"
+ return_code = subprocess.call(["afplay", audio_file])
+
+ print("audio played")
+
+ for x in range(8):
+  file1 = Path("test.mp3")
+  data = bytearray(file1.read_bytes())
+  flipbyte = random.randrange(0, len(data))
+  data[flipbyte] ^= 1 << random.randrange(0, 8)
+  file1.write_bytes(data)
+  print(x)
+
+ print("audio corrupted")
+
+ count += 1
+
+ print(count)
